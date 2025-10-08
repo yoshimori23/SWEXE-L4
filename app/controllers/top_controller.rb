7fe030094097a7dc610e4d4e2bc6c1c9fec74c1f
yoshimori <2311140023p@ed.fuk.kindai.ac.jp>
2025-10-08 08:56:45 +0000

    kisokadai2

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -8,11 +8,16 @@ class TopController < ApplicationController
     end
     
     def login
-        if params[:uid] == "kindai" and params[:pass] == "sanriko"
+        if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
             session[:login_uid] = params[:uid]
             redirect_to top_main_path
         else
             render "error", status: 422
         end
+    end
+    
+    def logout
+        session.delete(:login_uid)
+        redirect_to top_main_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'top/main'
   post 'top/login'
   root 'top#main'
+  get'top/logout'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
